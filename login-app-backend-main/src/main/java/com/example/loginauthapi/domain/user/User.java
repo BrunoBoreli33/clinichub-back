d920d7a911f05af5b3d182ba2bd4d447da4ad097
BrunoBoreli33 <contatobrunoboreli@gmail.com>
2025-09-30 13:16:58 -0300

    LoginRegister_Backend

---
+++ login-app-backend-main/src/main/java/com/example/loginauthapi/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.example.loginauthapi.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private boolean confirmed;
+    private String confirmationToken;
+    private String role;
+
+}
