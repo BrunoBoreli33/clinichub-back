d8289d295810db9b7e6778ac0b0f46fb9c9a1099
Alefe Oliveira <alefe.oliveira@datasmart.pt>
2025-10-06 11:09:37 -0300

    Cors config API deployed on Render

--- src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
+++ src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081") // Adicione esta linha
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "https://clinichub-back-latest.onrender.com")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
