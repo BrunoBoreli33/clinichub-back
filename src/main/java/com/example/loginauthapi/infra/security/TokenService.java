0b49f2181249617c971d69e765285ec006bd460e
Alefe Oliveira <alefe.oliveira@datasmart.pt>
2025-10-06 08:34:31 -0300

    App Env Config

--- src/main/java/com/example/loginauthapi/infra/security/TokenService.java
+++ src/main/java/com/example/loginauthapi/infra/security/TokenService.java
@@ -16,16 +16,15 @@ import java.time.ZoneOffset;
 public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
+
     public String generateToken(User user){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-
-            String token = JWT.create()
+            return JWT.create()
                     .withIssuer("login-auth-api")
                     .withSubject(user.getEmail())
                     .withExpiresAt(this.generateExpirationDate())
                     .sign(algorithm);
-            return token;
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error while authenticating");
         }
